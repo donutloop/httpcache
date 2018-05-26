4c42f691351d107a450fd1ea0aa8e3d674a4c02d
Marcel Edmund Franke <marcel.edmund.franke@gmail.com>
2018-05-26 16:14:12 +0200

    Benchmark test for proxy

--- integration_tests/api_test.go
+++ integration_tests/api_test.go
@@ -3,6 +3,7 @@ package integration_tests
 import (
 	"fmt"
 	"github.com/donutloop/httpcache/internal/xhttp"
+	"math/rand"
 	"net"
 	"net/http"
 	"net/http/httptest"
@@ -73,4 +74,44 @@ func TestProxy(t *testing.T) {
 	if resp.StatusCode != http.StatusOK {
 		t.Fatalf("status code is bad (%v)", resp.StatusCode)
 	}
+}
+
+func BenchmarkProxy(b *testing.B) {
+	servers := make([]*httptest.Server, 0)
+	for i := 0; i < 10; i++ {
+		handler := func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte(`{"data": "` + generateData(256) + `"}`))
+			return
+		}
+		server := httptest.NewServer(http.HandlerFunc(handler))
+		servers = append(servers, server)
+	}
+
+	b.N = 10
+
+	for n := 0; n < b.N; n++ {
+		req, err := http.NewRequest(http.MethodGet, servers[rand.Intn(9)].URL, nil)
+		if err != nil {
+			continue
+		}
+
+		resp, err := client.Do(req)
+		if err != nil {
+			continue
+		}
+
+		if resp.StatusCode != http.StatusOK {
+			b.Logf("status code is bad (%v)", resp.StatusCode)
+		}
+	}
+}
+
+func generateData(n int) string {
+	const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(b)
 }
