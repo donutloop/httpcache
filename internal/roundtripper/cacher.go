9ebd46c57c68b7b09363da4a6fefa9b25da45c65
Marcel Edmund Franke <marcel.edmund.franke@gmail.com>
2018-11-10 18:33:36 +0100

    cache: control cache by cache control headers

--- internal/roundtripper/cacher.go
+++ internal/roundtripper/cacher.go
@@ -8,6 +8,7 @@ import (
 	"github.com/donutloop/httpcache/internal/cache"
 	"net/http"
 	"net/http/httputil"
+	"strings"
 )
 
 type CacheTransport struct {
@@ -16,10 +17,57 @@ type CacheTransport struct {
 }
 
 func (t *CacheTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+
+
 	clonedRequest, err := makeHashFromRequest(req)
 	if err != nil {
 		return nil, err
 	}
+
+	cacheControlHeader := req.Header.Get("Cache-Control")
+	if cacheControlHeader != "" {
+		cacheControlHeaders := strings.Split(cacheControlHeader, ",")
+
+		hasMustRevalidate := containsHeaderValue(cacheControlHeaders, "must-revalidate")
+		hasNoCache := containsHeaderValue(cacheControlHeaders, "no-cache")
+		hasNoStore := containsHeaderValue(cacheControlHeaders, "no-store")
+
+		if hasMustRevalidate && hasNoCache {
+			t.Cache.Delete(clonedRequest)
+			proxyResponse, err := t.Transport.RoundTrip(req)
+			if err != nil {
+				return nil, errors.New(fmt.Sprintf("proxy couldn't forward request to destination server (%v)", err))
+			}
+			cachedResponse := &cache.CachedResponse{Resp: proxyResponse}
+			t.Cache.Set(clonedRequest, cachedResponse)
+			return proxyResponse, nil
+		} else if hasNoCache {
+			proxyResponse, err := t.Transport.RoundTrip(req)
+			if err != nil {
+				return nil, errors.New(fmt.Sprintf("proxy couldn't forward request to destination server (%v)", err))
+			}
+			cachedResponse := &cache.CachedResponse{Resp: proxyResponse}
+			t.Cache.Set(clonedRequest, cachedResponse)
+			return proxyResponse, nil
+		} else if hasMustRevalidate {
+			t.Cache.Delete(clonedRequest)
+			proxyResponse, err := t.Transport.RoundTrip(req)
+			if err != nil {
+				return nil, errors.New(fmt.Sprintf("proxy couldn't forward request to destination server (%v)", err))
+			}
+			cachedResponse := &cache.CachedResponse{Resp: proxyResponse}
+			t.Cache.Set(clonedRequest, cachedResponse)
+			return proxyResponse, nil
+		} else if hasNoStore {
+			t.Cache.Delete(clonedRequest)
+			proxyResponse, err := t.Transport.RoundTrip(req)
+			if err != nil {
+				return nil, errors.New(fmt.Sprintf("proxy couldn't forward request to destination server (%v)", err))
+			}
+			return proxyResponse, nil
+		}
+	}
+
 	cachedResponse, ok := t.Cache.Get(clonedRequest)
 	if !ok {
 		proxyResponse, err := t.Transport.RoundTrip(req)
@@ -53,4 +101,13 @@ func makeHashFromRequest(r *http.Request) (string, error) {
 	hasher := md5.New()
 	hasher.Write([]byte(d))
 	return hex.EncodeToString(hasher.Sum(nil)), nil
+}
+
+func containsHeaderValue(headerValues []string, value string) bool {
+	for _, v := range headerValues {
+		if v == value {
+			return true
+		}
+	}
+	return false
 }
